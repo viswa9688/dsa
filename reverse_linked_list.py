301fa9d5c8e440dfb9bbf4d820e50215ef06678e
viswateja108 <viswa968@gmail.com>
2025-04-01 14:26:35 +0530

    py solution file push

---
+++ reverse_linked_list.py
@@ -0,0 +1,16 @@
+# Definition for singly-linked list node
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+# Function to reverse the linked list
+def reverse(head):
+    prev = None
+    current = head
+    while current is not None:
+        next_node = current.next  # Store the next node
+        current.next = prev       # Reverse the link
+        prev = current            # Move prev one step forward
+        current = next_node       # Move current one step forward
+    return prev
